035bb39f7f3826e761377d025883298081ae2be1
Alexandro Cristian Schumann Carraro <Alexandro.Carraro@cmaritima.com.br>
2024-07-18 17:10:12 -0300

    fisrt commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+window.revelar = ScrollReveal({reset:true});
+
+revelar.reveal('.section-slide-container', {
+    origin: 'right', 
+    duration: 5000,
+    opacity: 0,
+});
+
+revelar.reveal('.brief-description-container-txt', {
+    origin: 'right', 
+    distance: '100%', 
+    duration: 5000,
+    opacity: 0,
+});
+
+revelar.reveal('.section-products-container-title', {
+    origin: 'left', 
+    distance: '100%', 
+    duration: 1000,
+    opacity: 0,
+});
+
+revelar.reveal('.item1, .item2, .item3, .item4, .item5, .item6, .item7, .item8, .item9, .item10, .item11, .item12, .item13, .item14, .item15, .item15, .item16, .item17, .item18, .item19, .item20', {
+    duration: 2000,
+    opacity: 0,
+});
+
+///
+
+function openWhatsApp() {
+    const telefone = '5547996144717';
+    const mensagem = 'Ol%C3%A1!%20Cheguei%20at%C3%A9%20aqui%20por%20meio%20do%20site,%20e%20gostaria%20de%20falar%20sobre%20os%20produtos!';
+    const url = `https://wa.me/${telefone}?text=${mensagem}`;
+    window.open(url, '_blank');
+}
