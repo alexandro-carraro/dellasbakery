afbdfdb100899bd0a57b8eb874383a2776e090a8
Alexandro Cristian Schumann Carraro <Alexandro.Carraro@cmaritima.com.br>
2024-07-18 18:12:22 -0300

    atualizacao de overflow

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@ window.revelar = ScrollReveal({reset:true});
 
 revelar.reveal('.section-slide-container', {
     origin: 'right', 
+    distance: '100%', 
     duration: 5000,
     opacity: 0,
 });
 
 revelar.reveal('.brief-description-container-txt', {
     origin: 'right', 
-    distance: '100%', 
+    distance: '80%', 
     duration: 5000,
     opacity: 0,
 });
@@ -21,7 +22,7 @@ revelar.reveal('.section-products-container-title', {
 });
 
 revelar.reveal('.item1, .item2, .item3, .item4, .item5, .item6, .item7, .item8, .item9, .item10, .item11, .item12, .item13, .item14, .item15, .item15, .item16, .item17, .item18, .item19, .item20', {
-    duration: 2000,
+    duration: 1000,
     opacity: 0,
 });
 
